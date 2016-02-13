90cb7b2f3116b183c010565aed505bd3e7504a9e
nyghtrocker <nadeem.nasir@live.com>
2016-02-13 22:32:25 +0100

    webapi confi

---
+++ Learning.Web/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using Learning.Data;
+using Learning.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Learning.Web.Controllers
+{
+    public class CoursesController : ApiController
+    {
+        public List<Course> Get()
+        {
+            ILearningRepository repository = new LearningRepository(new LearningContext());
+
+            return repository.GetAllCourses().ToList();
+        }
+
+        public Course GetCourse(int id)
+        {
+            ILearningRepository repository = new LearningRepository(new LearningContext());
+
+            return repository.GetCourse(id);
+        }
+    }
+}
